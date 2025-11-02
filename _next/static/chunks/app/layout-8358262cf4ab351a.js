f42f66dfe0bf5b967f9d54aa48230632c3dc19f2
DiogoCastroSilva <DiogoCastroSilva@users.noreply.github.com>
2025-11-02 02:31:24 +0000

    deploy: 76e28bdb02006988c6e6bcc13e2a9c8d77c3f8db

---
+++ _next/static/chunks/app/layout-8358262cf4ab351a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{77:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2028,23))},2028:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[785,587,315,358],()=>s(77)),_N_E=e.O()}]);
