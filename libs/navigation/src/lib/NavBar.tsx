f618c4a38049ad25d21e576b0bd1b6ef55d56ee8
DiogoCastroSilva <diogocastrodiassilva@gmail.com>
2026-04-11 15:53:39 -0700

    feat(navigation): Improve navigation library

--- libs/navigation/src/lib/NavBar.tsx
+++ libs/navigation/src/lib/NavBar.tsx
@@ -1,18 +1,24 @@
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-];
+'use client';
 
-export function NavBar() {
+import Link from 'next/link';
+import { navigation } from './config.js';
+
+type NavBarProps = {
+  currentPath?: string;
+};
+
+export function NavBar({ currentPath }: NavBarProps) {
   return (
-    <nav className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+    <nav className="relative after:pointer-events-none after:absolute after:inset-x-0 ">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <a href="#" aria-current="page" className="text-white">
+              <a
+                href="#"
+                aria-current="page"
+                className="dark:text-white hover:text-cyan-200"
+              >
                 Kathleen Miller
               </a>
             </div>
@@ -20,14 +26,14 @@ export function NavBar() {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="flex space-x-4">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
                   href={item.href}
-                  aria-current={item.current ? 'page' : undefined}
-                  className="dark:text-gray-800"
+                  aria-current={currentPath === item.href ? 'page' : undefined}
+                  className="dark:text-white hover:text-cyan-200"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
---
+++ libs/navigation/src/lib/config.ts
@@ -0,0 +1,10 @@
+type NavigationItem = {
+  name: string;
+  href: string;
+};
+
+export const navigation: NavigationItem[] = [
+  { name: 'Posts', href: '/' },
+  { name: 'Publications', href: '/publications' },
+  { name: 'About', href: '/about' },
+];
