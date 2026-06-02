5f1283fba61a50539de486a1c761978ed31fb972
DiogoCastroSilva <diogocastrodiassilva@gmail.com>
2026-06-01 18:23:39 -0700

    fix(packages): fix post location/imports

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -10,6 +10,7 @@ const createMDX = require('@next/mdx');
 const nextConfig = {
   output: 'export', // For GitHub Pages
   distDir: '../../public',
+  transpilePackages: ['@katie-portofolio/posts', '@katie-portofolio/navigation'],
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   experimental: {
     mdxRs: true, // use Rust-based MDX compiler (faster)
