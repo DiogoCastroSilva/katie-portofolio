cf7e4c9196fc6ae08322a68a45d3e05a88137a3e
DiogoCastroSilva <diogocastrodiassilva@gmail.com>
2026-06-01 19:42:57 -0700

    feat(about): adjust reponsiveness and new image

--- apps/main/src/app/ui/AboutSection.tsx
+++ apps/main/src/app/ui/AboutSection.tsx
@@ -2,7 +2,7 @@
 import { motion } from 'motion/react';
 import Image from 'next/image';
 
-import profileImage from '../../assets/profile.svg';
+import profileImage from '../../assets/profile.jpeg';
 
 const PROFILE_PIC_SIZE = 400;
 const ABOUT_CONTENT = `Highly analytical and results-oriented professional with extensive experience in optimization and network modelling, strategy development, and data analysis. Demonstrated ability to define and implement innovative processes and solutions, collaborate with diverse stakeholders, and manage complex analytical models. Passionate about developing data-driven solutions to support climate change mitigation and adaptation strategies.`;
@@ -26,14 +26,14 @@ export const AboutSection = () => {
             className="h-42 w-42 rounded-full sm:h-62 sm:w-62 md:h-30 md:w-30 lg:h-62 lg:w-62"
           />
           <div className="flex flex-col items-center justify-center text-wrap">
-            <h1 className="text-2xl lg:text-3xl font-bold">Kathleen Miller</h1>
+            <h1 className="text-2xl font-bold lg:text-3xl">Kathleen Miller</h1>
             <p className="items-center text-center text-lg text-gray-600 dark:text-gray-400">
               Passionate about science and nature-based solutions
             </p>
           </div>
         </div>
         <div className="flex flex-12 flex-col gap-2 pt-2">
-          <h2 className="text-xl lg:text-2xl font-bold">About Me</h2>
+          <h2 className="text-xl font-bold lg:text-2xl">About Me</h2>
           <p className="mt-4 indent-4 text-lg/8 text-gray-800 dark:text-gray-400">
             {ABOUT_CONTENT}
           </p>
